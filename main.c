479e93eb161815461f2b858cec5e55fd18d26110
Kenton A. Hoover <kahoover@mail.site601.com>
2017-07-30 17:11:55 -0400

    Initial main.c
    This is very much a rough out at this stage. Lots more detail to insert.
    
    Signed-off-by: Kenton A. Hoover <kahoover@mail.site601.com>

---
+++ main.c
@@ -0,0 +1,65 @@
+localelapsedsec,ntptime,solarvoltage,batteryvoltage,batteryfull,chargeractive,water_detect,water_ingress,accel_x,accel_y,accel_z,compass_heading,pitch,roll,yaw,pool_temp
+
+
+#define	REFVB	?	// battery voltage bridge pin
+#define	REDVS	?	// solar voltage bridge pin
+#define	CHARGERACTIVE	?	// charger LED out pin
+#define	INWATER	?	// in water current sense
+#define	INGRESS	?	// ingress sensing on bottom phone
+#define	ACCEL_I2C_ADDR	?	// address of acceleromter
+#define	GYRO_I2C_ADDR	?	// address of gyrolocalelapsedsec,ntptime,solarvoltage,batteryvoltage,batteryfull,chargeractive,water_detect,water_ingress,accel_x,accel_y,accel_z,compass_heading,pitch,roll,yaw,pool_temp
+
+
+#define	REFVB	?	// battery voltage bridge pin
+#define	REDVS	?	// solar voltage bridge pin
+#define	CHARGERACTIVE	?	// charger LED out pin
+#define	INWATER	?	// in water current sense
+#define	INGRESS	?	// ingress sensing on bottom phone
+#define	ACCEL_I2C_ADDR	?	// address of acceleromter
+#define	GYRO_I2C_ADDR	?	// address of gyro
+#define	REPORTINGSITE	"stats.fortalcatraz.org"
+#define	REPORTINGURL	"http://stats.fortalcatraz.org/bfp/poolstation"
+
+void setup() {
+// set modes on all pins
+
+// initialize temp reader
+
+// get time via NTP
+
+}
+
+void loop() {
+// check elapsed time vs
+
+// read temp
+// check solar voltage
+// check battery voltage
+// check water
+// read accellerometer
+// read compass heading
+// read 
+}
+#define	REPORTINGSITE	"stats.fortalcatraz.org"
+#define	REPORTINGURL	"http://stats.fortalcatraz.org/bfp/poolstation"
+
+void setup() {
+// set modes on all pins
+
+// initialize temp reader
+
+// get time via NTP
+
+}
+
+void loop() {
+// check elapsed time vs
+
+// read temp
+// check solar voltage
+// check battery voltage
+// check water
+// read accellerometer
+// read compass heading
+// read 
+}
